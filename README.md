e22ad82362631739dccb6dcd453bc7cdf7b5cd42
Harsha <Harshavardhan.padidala@sleepnumber.com>
2022-11-07 20:50:57 -0800

    code changes with command line args

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Harshavardhan Padidala - 015219945
 
 ## CMPE-279 Assignment 1
 ### Assignment 1 - Extend the server code to use privilege separation
+
+## CMPE-279 Assignment 2
+### Assignment 2 - Re-exec the server’s child process after forking.
--- assignment2/server.c
+++ assignment2/server.c
@@ -6,67 +6,91 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <pwd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
-    struct passwd* pwd;
-    pid_t pid;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
     char *user = "nobody";
+    struct passwd *pwd;
+    pid_t pid;
+
 
     printf("execve=0x%p\n", execve);
 
-    // Creating socket file descriptor
+    if(strcmp(argv[0],"socket")==0){
+        int newSoc = atoi(argv[1]);
+        valread = read(newSoc, buffer, 1024);
+        printf("%s\n", buffer);
+        send(newSoc, hello, strlen(hello),0);
+        printf("Hello Message sent!!! \n");
+        exit(0);
+    }
+
+    
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+  
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt)))
+
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
     {
-        perror("Bind Failed");
+        perror("bind failed");
         exit(EXIT_FAILURE);
     }
     if (listen(server_fd, 3) < 0)
     {
-        perror("Listen");
+        perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0)
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0)
     {
-        perror("Accept");
+        perror("accept");
         exit(EXIT_FAILURE);
-    }    
+    }
     pid = fork();
-    if (pid == 0) {
-        if ((pwd = getpwnam(user)) == NULL) {
-            perror("No UID found for the user nobody");
+    if (pid == 0)
+    {
+        if ((pwd = getpwnam(user)) == NULL)
+        {
+            perror("Cannot find UID");
         }
         setuid(pwd->pw_uid);
-        valread = read(new_socket , buffer, 1024);
+        valread = read(new_socket, buffer, 1024);
         printf("%s\n", buffer);
-        send(new_socket , hello , strlen(hello) , 0);
+        send(new_socket, hello, strlen(hello), 0);
         printf("Hello message sent\n");
+
+        
+        int duplicateSocket = dup(new_socket);
+        if(duplicateSocket==-1)
+            perror("Duplicate failed");
+
+    
+        char socketString[10];
+        sprintf(socketString, "%d", duplicateSocket);
+        char *args[] = {"socket", socketString, NULL};
+        execvp(argv[0], args);
         exit(0);
     }
     wait(NULL);
